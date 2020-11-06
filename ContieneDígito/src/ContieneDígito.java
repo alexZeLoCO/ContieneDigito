b66aec211f4f1fce2adc6a80450284b6f50ea572
alexZeLoCO <alejandro.rod.lop@gmail.com>
2020-11-06 10:47:14 +0100

    Segundo Commit

--- ContieneDígito/src/ContieneDígito.java
+++ ContieneDígito/src/ContieneDígito.java
@@ -10,12 +10,12 @@ public class ContieneDígito {
 		int valor=teclado.nextInt();		//ALMACENA VALOR EN VALOR
 		System.out.print("Introduzca una número: ");		//SOLICITA SUCESIÓN
 		n=teclado.nextInt();				//ALMACENA NÚMERO EN N
-		digito=n%10;				//DIGITO = SIGUIENTE DIGITO DE LA SUCESIÓN
+		
 		do {
+			digito=n%10;		//COGER SIGUIENTE VALOR
 			n=n/10;		//QUITAR UN DIGITO A N
-			digito=n%10;		//COGER SIGUIENTE VALOR		
-		} while (valor!=digito && n%10!=0); 		//CONDICIÓN
-		if (n/10==0) {		//SI SE LA LLEGADO AL FINAL DEL NÚMERO
+		} while (valor!=digito && n!=0); 		//CONDICIÓN
+		if (n==0) {		//SI SE LA LLEGADO AL FINAL DEL NÚMERO
 			System.out.print("La secuencia no contiene el número introducido.");		//OUTPUT
 		} else {		//SI NO (EL BUCLE SE ROMPIÓ POR ENCONTRAR DOS VALORES IGUALES)
 			System.out.print("La secuencia contiene el número introducido.");		//OUTPUT
